81f7692518d495cd0b7f40a0421490047d3fdf3f
Leonardo Martins <60331681+Yunkzinn@users.noreply.github.com>
2023-02-01 01:46:57 -0300

    Update recon-sqli.sh

--- recon-sqli.sh
+++ recon-sqli.sh
@@ -3,19 +3,19 @@ echo "By https://twitter.com/V24016770"
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 red=`tput setaf 1`
-mkdir $1
-cd $1
+mkdir $1;
+cd $1;
 echo "${yellow}======> Started Sqli recon on $1"
-gau $1 |uro > passive-endpoints
+gau $1 |uro > passive-endpoints;
 echo "${green}======> passive-endpoints.txt saved"
 echo "${yellow}======> Endpoints parameters"
-cat passive-endpoints|grep ‘?’ >parameter-endpoint
+cat passive-endpoints|grep ‘?’ >parameter-endpoint;
 echo "${green}======> parameter-endpoint.txt saved"
 echo "${yellow}======> Qsreplace"
-cat parameter-endpoint|qsreplace \’ >sqli-test-endpoints
+cat parameter-endpoint|qsreplace \’ >sqli-test-endpoints;
 echo "${green}======> sqli-test-endpoints.txt saved"
 echo "${red}======> Potential entry points"
-httpx -l sqli-test-endpoints -ms “Query failed:”
+httpx -l sqli-test-endpoints -ms “Query failed:”;
 echo "${yellow}======> sqli-test-endpoints.txt saved"
-echo "Happy hacking by ${yellow}https://twitter.com/V24016770"
+echo "Happy hacking by ${yellow}https://twitter.com/ch4rs3t"
 echo "${red}======> Recon finished"
